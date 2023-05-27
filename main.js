c58f0c5c15a0fd231cf8c2ad3fb6702050894a60
recordlight <recordlight123@hotmail.com>
2023-05-27 22:48:41 +0800

    change of design

--- main.js
+++ main.js
@@ -28,10 +28,8 @@ client.on('message', (channel, tags, message, self) => {
 });
 
 
-function resetAnimation() {
-  dialogElement.style.animation = 'none';
-  dialogElement.offsetHeight; /* trigger reflow */
-  dialogElement.style.animation = null; 
+function resetAnimation() { 
+  animateCSS('.message', 'fadeInRight');
 }
 
 function getMessageHTML(message, { emotes }) {
@@ -71,3 +69,21 @@ function getMessageHTML(message, { emotes }) {
 
   return messageHTML;
 }
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
